a3170eb8c9fce3cc515e4d953e5f8868b6283fc9
Daniel Vega <daniel.vega03@hotmail.com>
2023-11-20 19:49:56 -0500

    implemntacion prueba auth de google

--- reciward-laravel/routes/web.php
+++ reciward-laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +18,32 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/** IMPLEMENTACION DE OAUTH DE GOOGLE */
+
+ 
+Route::get('/login-google', function () {
+    return Socialite::driver('google')->redirect();
+});
+ 
+Route::get('/google-callback', function () {
+    $user = Socialite::driver('google')->user();
+
+    $userExiste = User::where('auth_id', $user->id)->where('auth_name',
+        'google')->first();
+    if ($userExiste) {
+        Auth::login($userExiste);
+    } else {
+        $nuevoUsuario = User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => null,
+            'auth_id' => $user->id,
+            'auth_name' => 'google'
+        ]);
+        Auth::login($nuevoUsuario);
+    }
+    // $user->token
 });
