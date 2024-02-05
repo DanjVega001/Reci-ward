65eb8b6866372279747311b4177ed06401b3fb35
Anderson <127869546+AndersonMclovin@users.noreply.github.com>
2024-02-05 18:51:37 -0500

    clasificacion listo

--- front-angular/src/app/clasificacion/create/create.component.ts
+++ front-angular/src/app/clasificacion/create/create.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ClasificacionService } from '../../servicios/clasificacion.service'; // Asumiendo que tienes un servicio llamado 'clasificacion.service'
-import { Clasificacion } from '../../modelos/clasificacion.model'; // Asumiendo que tienes un modelo llamado 'clasificacion.model'
+import { ClasificacionService } from '../../servicios/clasificacion.service';
+import { Clasificacion } from '../../modelos/clasificacion.model';
 
 @Component({
   selector: 'app-create',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  providers: [ClasificacionService], // Cambiando el proveedor a ClasificacionService
+  providers: [ClasificacionService],
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss'] // Corrigiendo el nombre del atributo 'styleUrls'
+  styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
   id: string | null = null;
   clave: string | null = null;
-  listaClasificaciones: Clasificacion[] = []; // Cambiando el nombre de 'listaTips' a 'listaClasificaciones'
+  listaClasificaciones: Clasificacion[] = [];
 
   ClasificacionForm: FormGroup = this.fb.group({
     id: [''],
-    nombre_clasificacion: ['', Validators.required], // Agregando validador requerido
+    nombreClasificacion: ['', Validators.required],
     descripcion: [''],
   });
 
@@ -35,7 +35,7 @@ export class CreateComponent implements OnInit {
 
   ngOnInit(): void {
     this.validarToken();
-    this.cargarClasificaciones(); // Cambiando el nombre del método a 'cargarClasificaciones'
+    this.agregarClasificacion(); // Cambiando el nombre del método a 'cargarClasificaciones'
     this.verEditar();
   }
 
@@ -48,10 +48,10 @@ export class CreateComponent implements OnInit {
     }
   }
 
-  cargarClasificaciones(): void { // Cambiando el nombre del método a 'cargarClasificaciones'
+  agregarClasificacion(): void { // Cambiando el nombre del método a 'cargarClasificaciones'
     this.clasificacionService.getClasificaciones(this.clave).subscribe(
       data => {
-        this.listaClasificaciones = data; // Cambiando la asignación de datos a 'listaClasificaciones'
+        this.listaClasificaciones = data;
       },
       err => {
         console.log(err);
@@ -66,7 +66,7 @@ export class CreateComponent implements OnInit {
           data => {
             this.ClasificacionForm.setValue({
               id: data.id,
-              nombre_clasificacion: data.nombre_clasificacion,
+              nombreClasificacion: data.nombreClasificacion,
               descripcion: data.descripcion,
             });
           },
--- front-angular/src/app/modelos/clasificacion.model.ts
+++ front-angular/src/app/modelos/clasificacion.model.ts
@@ -1,10 +1,16 @@
+// Cambiando de 'nombre_clasificacion' a 'nombreClasificacio'
 export class Clasificacion {
-    id?:number;
-    nombreClasificacion?:string| null | undefined;
-nombre_clasificaciones: any;
-
-    constructor(id?:number, nombreClasificacion?:string){
-        this.id = id;
-        this.nombreClasificacion=nombreClasificacion;
+    id?: number;
+    nombreClasificacion: string | undefined | null;
+    descripcion: string | undefined | null;
+    admin_id?: number;
+  
+    constructor(id?: number, nombreClasificacion?: string, descripcion?: string, admin_id?: number) {
+      this.id = id;
+      this.nombreClasificacion = nombreClasificacion;
+      this.descripcion = descripcion;
+      this.admin_id = admin_id;
     }
-}
+  }
+  
+  
