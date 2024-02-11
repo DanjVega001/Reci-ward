d68279fdecce016f0e0ec440c2492d8ff7cdd37a
Edward Vega <edward.vega@correo.uis.edu.>
2024-02-11 10:59:25 -0500

    ver puntos hecho

--- reciward_flutter_app/lib/features/aprendiz/puntos/presentation/bloc/punto_bloc.dart
+++ reciward_flutter_app/lib/features/aprendiz/puntos/presentation/bloc/punto_bloc.dart
@@ -9,7 +9,6 @@ part 'punto_event.dart';
 part 'punto_state.dart';
 
 class PuntoBloc extends Bloc<PuntoEvent, PuntoState> {
-
   GetPuntosUsecase usecase = GetIt.instance<GetPuntosUsecase>();
 
   PuntoBloc() : super(PuntoInitial()) {
@@ -18,11 +17,12 @@ class PuntoBloc extends Bloc<PuntoEvent, PuntoState> {
 
   void onGetPuntosEvent(GetPuntosEvent event, Emitter<PuntoState> emit) async {
     try {
-      Either<DioException, GetPuntoDto> either = await usecase.call(event.accessToken);
-      return either.fold((dioException) => 
-        emit(GetPuntosFailedState(error: dioException.message!)), 
-      (data) => 
-        emit(GetPuntosSuccessState(getPuntoDto: data)));
+      Either<DioException, GetPuntoDto> either =
+          await usecase.call(event.accessToken);
+      return either.fold(
+          (dioException) =>
+              emit(GetPuntosFailedState(error: dioException.message!)),
+          (data) => emit(GetPuntosSuccessState(getPuntoDto: data)));
     } catch (e) {
       print("Error en onGetPuntosEvent ${e.toString()}");
       return emit(GetPuntosFailedState(error: e.toString()));
