66e91d5138edda7f5b447d576436dbc153030c11
AndersonMclovin <127869546+AndersonMclovin@users.noreply.github.com>
2024-02-23 09:26:50 -0500

    fondos

--- reciward_flutter_app/lib/features/aprendiz/bono/presentation/pages/home_bono_page.dart
+++ reciward_flutter_app/lib/features/aprendiz/bono/presentation/pages/home_bono_page.dart
@@ -21,61 +21,63 @@ class _HomeBonoPageState extends State<HomeBonoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Pallete.colorWhite,
       appBar: const AppBarReciward(),
-      body: Column(
-        children: [
-          const GetPuntosBanner(),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                _showTable = !_showTable;
-              });
-            },
-            child: Text(_showTable
-                ? '  Ocultar historial de bonos  '
-                : '  Mostrar historial de bonos  '),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/manual_images/arrrr.png"),
+            fit: BoxFit.cover,
           ),
-          const SizedBox(height: 10),
-          if (_showTable) ...[
-            const Text(
-              'Historial',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+        ),
+        child: Column(
+          children: [
+            const GetPuntosBanner(),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _showTable = !_showTable;
+                });
+              },
+              child: Text(_showTable ? '  Ocultar historial de bonos  ' : '  Mostrar historial de bonos  '),
             ),
             const SizedBox(height: 10),
-            Container(
-              width: 360,
-              decoration: BoxDecoration(
-                border: Border.all(color: Color.fromARGB(255, 84, 104, 59)),
-                color: const Color.fromARGB(255, 221, 221, 220),
-                borderRadius: BorderRadius.circular(10),
+            if (_showTable) ...[
+              const Text(
+                'Historial',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              child: BlocBuilder<BonoBloc, BonoState>(
-                builder: (context, state) {
-                  if (state is GetHistorialBonosSuccess) {
-                    final bonos = state.bonos;
-                    return Table(
-                      border: TableBorder.all(color: Colors.transparent),
-                      columnWidths: const {
-                        0: FixedColumnWidth(50),
-                        2: FixedColumnWidth(80), // Ajuste para hacer la columna "Estado" más delgada
-                        3: FlexColumnWidth(2), // Ajuste para hacer la columna "Caduca" un poco más ancha
-                      },
-                      defaultColumnWidth: const FixedColumnWidth(100),
-                      children: _buildTableRows(bonos),
-                    );
-                  }
+              const SizedBox(height: 10),
+              Container(
+                width: 360,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Color.fromARGB(255, 84, 104, 59)),
+                  color: const Color.fromARGB(255, 221, 221, 220),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: BlocBuilder<BonoBloc, BonoState>(
+                  builder: (context, state) {
+                    if (state is GetHistorialBonosSuccess) {
+                      final bonos = state.bonos;
+                      return Table(
+                        border: TableBorder.all(color: Colors.transparent),
+                        columnWidths: const {
+                          0: FixedColumnWidth(50),
+                          2: FixedColumnWidth(80), // Ajuste para hacer la columna "Estado" más delgada
+                          3: FlexColumnWidth(2), // Ajuste para hacer la columna "Caduca" un poco más ancha
+                        },
+                        defaultColumnWidth: const FixedColumnWidth(100),
+                        children: _buildTableRows(bonos),
+                      );
+                    }
 
-                  return Center(child: CircularProgressIndicator());
-                },
+                    return Center(child: CircularProgressIndicator());
+                  },
+                ),
               ),
-            ),
+            ],
           ],
-        ],
+        ),
       ),
       bottomNavigationBar: const NavReciward(
         currentIndex: 2,
