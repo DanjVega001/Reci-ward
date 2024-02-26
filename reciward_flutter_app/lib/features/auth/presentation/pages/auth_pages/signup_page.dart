5e99b1d83c70af184377f50d47c4c48239878f65
Oscar <oscarbueno.012345@gmail.com>
2024-02-26 09:23:02 -0500

    arreglo laravel crear ficha

--- reciward_flutter_app/lib/features/auth/presentation/pages/auth_pages/signup_page.dart
+++ reciward_flutter_app/lib/features/auth/presentation/pages/auth_pages/signup_page.dart
@@ -37,7 +37,6 @@ class _SignupPageState extends State<SignupPage> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       backgroundColor: Pallete.colorWhite,
       body: BlocConsumer<AuthBloc, AuthState>(
@@ -182,12 +181,10 @@ class _SignupPageState extends State<SignupPage> {
                           },
                           (fichas) {
                             if (fichas.isNotEmpty) {
-
                               if (selectedValueFicha.isEmpty) {
                                 selectedValueFicha =
                                     '${fichas[0].id}. ${fichas[0].numeroFicha} - ${fichas[0].nombreFicha}';
-                              } 
-                              
+                              }
                             }
                             return Container(
                               padding:
